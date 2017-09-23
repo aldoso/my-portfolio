3ac7bb752dda7f026f4f1f8b0fe5cc1c1b9552e0
aldoso <alexdobras@gmail.com>
2017-09-23 21:02:45 +0300

    layout improvement

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ $('#pills-tab a').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+
+$("#nav-resume-tab").click(function () {
+    $('#titlecover h5').text("My Resume")
+})
+
+$("#nav-portfolio-tab").click(function () {
+    $('#titlecover h5').text("My Latest Work")
+})
+
+$("#opa").click(function () {
+    //    $('#titlecover h5').text("My Resume")
+    $('#titlecover h5').text("My Resume")
+    $('#nav-portfolio-tab').removeClass("active")
+    $('#nav-resume-tab').addClass("active")
+
+})
