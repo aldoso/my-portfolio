7273dc3ce842dd3dfdb40d1feff67ff84d4039b8
aldoso <alexdobras@gmail.com>
2017-09-24 01:37:25 +0300

    added javascript for big buttons

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ $("#nav-portfolio-tab").click(function () {
     $('#titlecover h5').text("My Latest Work")
 })
 
-$("#opa").click(function () {
-    //    $('#titlecover h5').text("My Resume")
+$("#resume-button").click(function () {
     $('#titlecover h5').text("My Resume")
-    $('#nav-portfolio-tab').removeClass("active")
-    $('#nav-resume-tab').addClass("active")
+    $('#nav-resume').addClass("show").addClass("active")
+    $('#nav-portfolio').removeClass("active").removeClass("show")
+})
 
+$("#portfolio-button").click(function () {
+    $('#titlecover h5').text("My Latest Work")
+    $('#nav-resume').removeClass("show").removeClass("active")
+    $('#nav-portfolio').addClass("active").addClass("show")
 })
