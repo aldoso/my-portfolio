24a140b6238a8525e35d17fabe08f33f6ce77855
aldoso <alexdobras@gmail.com>
2017-09-24 14:36:58 +0300

    layout improvements

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ $('#pills-tab a').click(function (e) {
 
 
 $("#nav-resume-tab").click(function () {
-    $('#titlecover h5').text("My Resume")
+    $('#titlecover h6').text("My Resume")
 })
 
 $("#nav-portfolio-tab").click(function () {
-    $('#titlecover h5').text("My Latest Work")
+    $('#titlecover h6').text("My Latest Work")
+})
+
+
+$("#portfolio-button").click(function () {
+    $('#titlecover h6').text("My Latest Work")
+    $('#nav-resume').removeClass("show").removeClass("active")
+    $('#nav-portfolio').addClass("active").addClass("show")
 })
 
 $("#resume-button").click(function () {
-    $('#titlecover h5').text("My Resume")
-    $('#nav-resume').addClass("show").addClass("active")
-    $('#nav-portfolio').removeClass("active").removeClass("show")
-})
-
-$("#portfolio-button").click(function () {
-    $('#titlecover h5').text("My Latest Work")
-    $('#nav-resume').removeClass("show").removeClass("active")
-    $('#nav-portfolio').addClass("active").addClass("show")
+    $('#titlecover h6').text("My Resume")
+    $('#nav-portfolio, #nav-portfolio-tab').removeClass("active").removeClass("show")
+    $('#nav-resume, #nav-resume-tab').addClass("show").addClass("active")
 })
